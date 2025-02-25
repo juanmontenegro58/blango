d1a8c86a5d57fd50fdd3dc711b85cfe6eb483fc4
Juan Lowy <juanmontenegro58@gmail.com>
2025-02-25 16:00:50 +0000

    Finish Security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -186,6 +186,12 @@ class Dev(Configuration):
         "level": "DEBUG",
     },
     }
+  PASSWORD_HASHERS = [
+      'django.contrib.auth.hashers.Argon2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
 
 class Prod(Dev):
   DEBUG = values.BooleanValue(True)
