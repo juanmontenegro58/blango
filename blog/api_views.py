e6c112fa90dbdddb99f86d6e883a5e1d9b3a7569
Juan Lowy <juanmontenegro58@gmail.com>
2025-02-26 20:15:56 -0500

    Finish DRF views

--- blog/api_views.py
+++ blog/api_views.py
@@ -1,62 +1,86 @@
-import json
 from http import HTTPStatus
 
-from django.http import JsonResponse, HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from blog.models import Post
 from blog.api.serializers import PostSerializer
+from blog.models import Post
 
 
-def post_to_dict(post):
-    return {
-        "pk": post.pk,
-        "author_id": post.author_id,
-        "created_at": post.created_at,
-        "modified_at": post.modified_at,
-        "published_at": post.published_at,
-        "title": post.title,
-        "slug": post.slug,
-        "summary": post.summary,
-        "content": post.content,
-    }
+from rest_framework import mixins
+from rest_framework import generics
 
 
-@csrf_exempt
-def post_list(request):
+class PostList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+@api_view(["GET", "POST"])
+def post_list(request, format = None):
     if request.method == "GET":
         posts = Post.objects.all()
-        return JsonResponse({"data": PostSerializer(posts, many = True)})
+        return Response({"data": PostSerializer(posts, many=True).data})
     elif request.method == "POST":
-        post_data = json.loads(request.body)
-        serializer = PostSerializer(data=post_data)
-        serializer.is_valid(raise_exception=True)
-        post = serializer.save()
-        return JsonResponse(PostSerializer(post).data)
-        # return HttpResponse(
-        #     status=HTTPStatus.CREATED,
-        #     headers={"Location": reverse("api_post_detail", args=(post.pk,))},
-        # )
-
-    return HttpResponseNotAllowed(["GET", "POST"])
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            post = serializer.save()
+            return Response(
+                status=HTTPStatus.CREATED,
+                headers={"Location": reverse("api_post_detail", args=(post.pk,))},
+            )
+        return Response(serializer.errors, status=HTTPStatus.BAD_REQUEST)
 
 
-@csrf_exempt
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+# @api_view(["GET", "PUT", "DELETE"])
+# def post_detail(request, pk, format = None):
+#     try:
+#         post = Post.objects.get(pk=pk)
+#     except Post.DoesNotExist:
+#         return Response(status=HTTPStatus.NOT_FOUND)
 
-    if request.method == "GET":
-        return JsonResponse(post_to_dict(post))
-    elif request.method == "PUT":
-        post_data = json.loads(request.body)
-        serializer = PostSerializer(post, data=post_data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return HttpResponse(status=HTTPStatus.NO_CONTENT)
-    elif request.method == "DELETE":
-        post.delete()
-        return HttpResponse(status=HTTPStatus.NO_CONTENT)
+#     if request.method == "GET":
+#         return Response(PostSerializer(post).data)
+#     elif request.method == "PUT":
+#         serializer = PostSerializer(post, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(status=HTTPStatus.NO_CONTENT)
+#         return Response(serializer.errors, status=HTTPStatus.BAD_REQUEST)
+#     elif request.method == "DELETE":
+#         post.delete()
+#         return Response(status=HTTPStatus.NO_CONTENT)
 
-    return HttpResponseNotAllowed(["GET", "PUT", "DELETE"])
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+# class PostDetail(
+#     mixins.RetrieveModelMixin,
+#     mixins.UpdateModelMixin,
+#     mixins.DestroyModelMixin,
+#     generics.GenericAPIView,
+# ):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
