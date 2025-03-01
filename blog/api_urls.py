f7a34001ade0ed7fab3f6f308e334b978782985b
Juan Lowy <juanmontenegro58@gmail.com>
2025-03-01 11:22:22 -0500

    Finish filtering

--- blog/api_urls.py
+++ blog/api_urls.py
@@ -39,6 +39,11 @@ urlpatterns = [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path(
+        "posts/by-time/<str:period_name>/",
+        PostViewSet.as_view({"get": "list"}),
+        name="posts-by-time",
+    ),
     
 ]
 
